b35497896a143f3499adfc4fa6c8980b86d86d02
lazermortal <juanjoseflorezlopez@gmail.com>
2026-02-17 19:42:16 -0500

    creacion de la clase Persona

---
+++ src/personas/Persona.java
@@ -0,0 +1,40 @@
+package personas;
+
+import java.util.UUID;
+
+public abstract class Persona {
+    public String identificacion;
+    public String nombre;
+    public String contacto;
+
+    public Persona(UUID identificacion, String nombre, String contacto) {
+        this.identificacion = UUID.randomUUID().toString();
+        this.nombre = nombre;
+        this.contacto = contacto;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getContacto() {
+        return contacto;
+    }
+
+    public void setContacto(String contacto) {
+        this.contacto = contacto;
+    }
+    public abstract void mostrarInformacion();
+}
