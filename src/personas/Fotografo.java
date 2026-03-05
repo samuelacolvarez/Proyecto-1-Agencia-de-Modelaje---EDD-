714b82f5221cdee6c7be1bc74030e63759af6b15
lazermortal <juanjoseflorezlopez@gmail.com>
2026-03-05 13:31:57 -0500

    arreglo Ñ

--- src/personas/Fotografo.java
+++ src/personas/Fotografo.java
@@ -4,12 +4,12 @@ import java.util.UUID;
 
 public class Fotografo extends Persona {
     private String especialidad;
-    private int añosExperiencia;
+    private int aniosExperiencia;
 
-    public Fotografo(UUID identificacion, String nombre, String contacto, String especialidad, int añosExperiencia) {
+    public Fotografo(UUID identificacion, String nombre, String contacto, String especialidad, int aniosExperiencia) {
         super(identificacion, nombre, contacto);
         this.especialidad = especialidad;
-        this.añosExperiencia = añosExperiencia;
+        this.aniosExperiencia = aniosExperiencia;
     }
 
     public String getEspecialidad() {
@@ -20,17 +20,17 @@ public class Fotografo extends Persona {
         this.especialidad = especialidad;
     }
 
-    public int getAñosExperiencia() {
-        return añosExperiencia;
+    public int getAniosExperiencia() {
+        return aniosExperiencia;
     }
 
-    public void setAñosExperiencia(int añosExperiencia) {
-        this.añosExperiencia = añosExperiencia;
+    public void setAniosExperiencia(int aniosExperiencia) {
+        this.aniosExperiencia = aniosExperiencia;
     }
 
     public void mostrarInformacion() {
         IO.println("Especialidad: " + this.especialidad);
-        IO.println("Años de experiencia "+ this.añosExperiencia);
+        IO.println("Años de experiencia "+ this.aniosExperiencia);
         System.out.println("Nombre: " + this.nombre);
         System.out.println("Contacto: " + this.contacto);
         IO.println("Identificacion: " + this.identificacion);
